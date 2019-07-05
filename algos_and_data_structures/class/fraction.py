076730cff4f4f072704f5f7b9a76a016cf48b139
U-CISCO\phidixon <phidixon@PHIDIXON-YJY2G.cisco.com>
2019-07-05 09:01:19 -0400

    add graphs and fraction clas

---
+++ algos_and_data_structures/class/fraction.py
@@ -0,0 +1,25 @@
+class Fraction:
+    def __init__(self, top, bottom):
+        self.num = top
+        self.den = bottom
+
+    def show(self):
+        print(self.num, "/", self.den)
+
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __add__(self, other_fraction):
+        new_num = self.num*other_fraction.den + self.den*other_fraction.num
+        new_den = self.den * other_fraction.den
+        common = self.gcd(new_num, new_den)
+        return Fraction(new_num//common, new_den//common)
+
+    def gcd(self, m, n):
+        while m%n != 0:
+            old_m = m
+            old_n = n
+
+            m = old_n
+            n = old_m%old_n
+        return n
